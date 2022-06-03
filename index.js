2670eb4c3fc8f3a9ef66702fd6107689bb2268d1
Sheikh MD Sazidul Islam <sheikhmdsazidulislam@gmail.com>
2022-06-03 10:39:48 +0600

    added multiple data and single data fetching api

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require('cors');
 require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
 app.use(cors())
@@ -12,6 +12,34 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.5iwam.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+async function run(){
+    try{
+        await client.connect();
+        const servicesCollection = client.db("geniusCar").collection("services");
+        app.get('/service', async (req, res)=>{
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+        app.get('/service', async (req, res)=>{
+            const query = {};
+            const cursor = servicesCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services)
+        })
+        app.get('/service/:id', async (req, res)=>{
+            const id = req.params.id;
+            const query = {_id: ObjectId(id)};
+            const service = await servicesCollection.findOne(query);
+            res.send(service)
+        })
+    }
+    finally{
+
+    }
+}
+run().catch(console.dir)
 app.get('/', (req, res)=>{
     res.send('Running Genius Server')
 })
